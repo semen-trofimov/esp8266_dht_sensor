ae4b74bd9f82cee69b29f5a631f8ac7e9c68103b
semen.trofimov <semiony@gmail.com>
2020-03-22 16:52:54 +0300

    add version3

--- ESP8266_DHT.ino
+++ ESP8266_DHT.ino
@@ -16,15 +16,47 @@ char auth[] = "6b5c3d556cea42c5b7843708a0007830";
 char ssid[] = "asus";
 char pass[] = "t9indigo";
 
+#define DHTPIN 2          // Digital pin 4
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11     // DHT 11
+//#define DHTTYPE DHT22   // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
+
+DHT dht(DHTPIN, DHTTYPE);
+SimpleTimer timer;
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void sendSensor()
+{
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, h);  //V5 is for Humidity
+  Blynk.virtualWrite(V6, t);  //V6 is for Temperature
+}
+
 void setup()
 {
-  // Debug console
-  Serial.begin(9600);
-
+  Serial.begin(9600); // See the connection status in Serial Monitor
   Blynk.begin(auth, ssid, pass);
+
+  dht.begin();
+
+  // Setup a function to be called every second
+  timer.setInterval(1000L, sendSensor);
 }
 
 void loop()
 {
-  Blynk.run();
+  Blynk.run(); // Initiates Blynk
+  timer.run(); // Initiates SimpleTimer
 }
